625618c224366e3ff5ad90f450d807a488d19a40
yixiangguo <yixiangguo@hyperchain.com>
2018-09-04 11:24:44 +0800

    api address

--- target/test-classes/solidity.source/c3.sol
+++
@@ -1,9 +0,0 @@
-contract MyContract{
-                    function mreturns(uint a,bytes32 b,address c)returns(uint,bytes32[],address){
-                        bytes32[] bb;
-                        bb.push(b);
-                        bb.push(b);
-                        bb.push(bb);
-                        return (a,bb,c);
-                    }
-                }
